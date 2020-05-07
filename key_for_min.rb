07d53bc45c5b8eb04342c6c0af72f5b3ebad31c5
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-07 18:36:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,9 @@ def key_for_min_value(name_hash)
   lowest_key = ""
   name_hash.each do |key, value|
     if value < lowest_value
-      value = lowest_value
-      key = lowest_key
+      lowest_value = value
+      lowest_key = key
     end
   end
+  lowest_key
 end
